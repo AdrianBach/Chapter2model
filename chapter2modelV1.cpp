c97d9c7111c8569865c6fd2a88afc49dad61f53b
Adrian Bach <adrian.bach@stir.ac.uk>
2021-12-07 13:45:41 +0100

    New functions to write results into a table

--- chapter2modelV1.cpp
+++ chapter2modelV1.cpp
@@ -1,14 +1,20 @@
-#include <iostream>
-#include <string>
-using namespace std;
-// #include <list>
+#include <iostream>  // input and output from the console
+#include <string>    // manipulate char strings
+#include <fstream>   // handle files
+using namespace std; // basic functions
 
-/* declare global variables here */
+/* ---------------------------- Global variables ---------------------------- */
+
+/* give a name to the simulation */
+// hard coded NOT GOOD
+string simulationName = "test";
 
 /* define animals and resources modelled */
-int predatorNb = 1;           // Number of predators modelled
-int preyNb = 2;               // Number of preys modelled
-int resourceNb = 2;           // Number of resources available
+// hard coded NOT GOOD
+int predatorNb = 1; // Number of predators modelled
+int preyNb = 2;     // Number of preys modelled
+int resourceNb = 2; // Number of resources available
+
 string *predatorTypes;        // types of predator simulated: predator1..n prey1..n
 string *preyTypes;            // prey1..n
 string *primaryResourceTypes; // resource1..n
@@ -17,22 +23,45 @@ string *primaryResourceTypes; // resource1..n
 int *predatorsInitialDensities;
 int *preysInitialDensities;
 
+/* world size */
+int worldSize; // side of the squared lanscape in number of cells [0;+inf[
+
+/* time step variable */
+int timeStep;
+
+/* ---------------------- Some useful global functions ---------------------- */
+
+int sumColumn(int **table, int maxRow, int columnIndex) // sum of row values in a column
+{
+    int sum = 0;
+    int r = 0;
+    while (r < maxRow)
+    {
+        sum += table[r][columnIndex];
+        r++;
+    }
+
+    return sum;
+}
+
+/* ----------------------------- Object classes ----------------------------- */
+
 /*
-create a class for landscape:
+create a class:
 - all the variables that define it
 - all the functions that manipulate these variables
 - make sure that the values can be used by other classes
 */
 
-/* create class */
 class landscape
 {
     /* list of landscape-specific variables */
-private:             // variables that should not be modified directly, nor accessed from the main function
-    int Size;        // side of the squared lanscape in number of cells [0;+inf[
-    int MaxResource; // max amount of resources on a cell
-    int rowNb;       // row number
-    int columnNb;    // column number
+private:                  // variables that should not be modified directly, nor accessed from the main function
+    int Size;             // side of the squared lanscape in number of cells [0;+inf[
+    int MaxResource;      // max amount of resources on a cell
+    int rowNb;            // row number
+    int columnNb;         // column number
+    fstream resultsTable; // file to write results in
 
 protected:                   // variables that should not be modified directly, nor accessed from the main function, but accessible to the other classes
     int **landscapeTablePtr; // pointer to the landscape table
@@ -100,7 +129,7 @@ public:                                  // functions that can modify the privat
                 // landscapeTablePtr[r][2 + resourceNb + prey] = 0;
 
                 /* a specific number to check if the info is where expected */
-                landscapeTablePtr[r][2 + resourceNb + prey] = 1+prey;
+                landscapeTablePtr[r][2 + resourceNb + prey] = 1 + prey;
 
             r++;
         }
@@ -111,9 +140,9 @@ public:                                  // functions that can modify the privat
         {
             for (int pred = 0; pred < predatorNb; pred++)
                 // landscapeTablePtr[r][2+ resourceNb + 2*preyNb + pred] = 0;
-                
+
                 /* a specific number to check if the info is where expected */
-                landscapeTablePtr[r][2+ resourceNb + 2*preyNb + pred] = 11+pred;
+                landscapeTablePtr[r][2 + resourceNb + 2 * preyNb + pred] = 11 + pred;
 
             r++;
         }
@@ -126,9 +155,21 @@ public:                                  // functions that can modify the privat
         {
             for (int res = 0; res < resourceNb; res++)
                 // landscapeTablePtr[r][2 + res] = MaxResource;
-                
+
                 /* a specific number to check if it works as expected */
-                landscapeTablePtr[r][2 + res] = 10*MaxResource;
+                landscapeTablePtr[r][2 + res] = 10 * MaxResource;
+
+            r++;
+        }
+    }
+
+    void resetCatches() // function to reset catches to zero
+    {
+        int r = 0;
+        while (r < rowNb)
+        {
+            for (int prey = 0; prey < preyNb; prey++)
+                landscapeTablePtr[r][2 + resourceNb + preyNb + prey] = 0;
 
             r++;
         }
@@ -144,7 +185,10 @@ public:                                  // functions that can modify the privat
         for (int i = 0; i < resourceNb; i++)
             cout << primaryResourceTypes[i] << " ";
         for (int i = 0; i < preyNb; i++)
-            cout << preyTypes[i] << " " << preyTypes[i] << "catches"
+            cout << preyTypes[i]
+                 << " ";
+        for (int i = 0; i < preyNb; i++)
+            cout << preyTypes[i] << "catches"
                  << " ";
         for (int i = 0; i < predatorNb; i++)
             cout << predatorTypes[i] << " ";
@@ -162,6 +206,72 @@ public:                                  // functions that can modify the privat
         }
     }
 
+    void createResultsTable(string name) // creates a resultsTable and returns a file object and a name
+    {
+
+        /* write headers */
+        resultsTable.open(name, ios::out);
+        if (resultsTable.is_open())
+        {
+            resultsTable << "timeStep"
+                         << ",";
+            for (int i = 0; i < resourceNb; i++)
+                resultsTable << primaryResourceTypes[i] << "amount"
+                             << ",";
+            for (int i = 0; i < preyNb; i++)
+                resultsTable << preyTypes[i] << "PopulationSize"
+                             << ",";
+            for (int i = 0; i < preyNb; i++)
+                resultsTable << preyTypes[i] << "catches"
+                             << ",";
+            for (int i = 0; i < predatorNb; i++)
+            {
+                resultsTable << predatorTypes[i] << "PopulationSize";
+                if (i == (predatorNb - 1))
+                    resultsTable << "\n";
+                else
+                    resultsTable << ",";
+            }
+
+            /* close file when finished */
+            resultsTable.close();
+        }
+    }
+
+    void saveMeasures(string name)
+    {
+
+        resultsTable.open(name, ios::app);
+        if (resultsTable.is_open())
+        {
+            resultsTable << timeStep
+                         << ",";
+
+            /* write the sum of the measure columns */
+
+            for (int i = 0; i < resourceNb; i++)
+                resultsTable << sumColumn(landscapeTablePtr, rowNb, 2 + i)
+                             << ",";
+            for (int i = 0; i < preyNb; i++)
+                resultsTable << sumColumn(landscapeTablePtr, rowNb, 2 + resourceNb + i)
+                             << ",";
+            for (int i = 0; i < preyNb; i++)
+                resultsTable << sumColumn(landscapeTablePtr, rowNb, 2 + resourceNb + preyNb + i)
+                             << ",";
+            for (int i = 0; i < predatorNb; i++)
+            {
+                resultsTable << sumColumn(landscapeTablePtr, rowNb, 2 + resourceNb + 2 * preyNb + i);
+                if (i == (predatorNb - 1))
+                    resultsTable << "\n";
+                else
+                    resultsTable << ",";
+            }
+
+            /* close file when finished */
+            resultsTable.close();
+        }
+    }
+
     void freeMemory() // free memory allocated to landscape table
     {
 
@@ -175,6 +285,8 @@ public:                                  // functions that can modify the privat
         landscapeTablePtr = NULL; // erase the address of the array of pointers to rows
     }
 };
+
+/* ------------------------------ Main program ------------------------------ */
 
 int main()
 {
@@ -199,8 +311,12 @@ int main()
     primaryResourceTypes[0] = "resource1"; // hard coded NOT GOOD
     primaryResourceTypes[1] = "resource2"; // hard coded NOT GOOD
 
+    worldSize = 3; // hard coded NOT GOOD
+
+    /* intiate world */
+
     /* contruct and create landscape */
-    landscape world(3, 10);
+    landscape world(worldSize, 10);
     world.create();
 
     /* check if everything is where expected */
@@ -209,6 +325,23 @@ int main()
     /* check fill function */
     world.fill();
     world.getInfo();
+
+    /* check resetCatches function */
+    world.resetCatches();
+    world.getInfo();
+
+    /* create a results csv file */
+
+    /* Build name */
+    string resultsTableName = simulationName + "ResultsTable.csv";
+    world.createResultsTable(resultsTableName);
+
+    /* start simulation */
+
+    timeStep = 0;
+
+    /* check measure function */
+    world.saveMeasures(resultsTableName);
 
     /* free memory */
     world.freeMemory();
