4d41de9d087131b41367c863911583527b76af34
Adrian Bach <adrian.bach@stir.ac.uk>
2022-04-15 18:03:17 +0100

    Folder ready for small simulation batch
    Hi @bradduthie ,
    
    Hope you are enjoying your good Friday !
    
    Absolutely no rush to do this. I have prepared the "findEquilibrium" folder to run simulations across a few preys maxConsume and predator catchProbability parameter values. Everything should be ready to run, you will just have to cd to the "findEquilibrium" folder and execute < bash ./findEq-launchReplicatesSim.sh >.
    
    Have a good weekend !
    
    Cheers,
    Adrian

--- chapter2-Rstats/statsResultsTable.R
+++ chapter2-Rstats/statsResultsTable.R
@@ -1,3 +1,5 @@
+library(ggplot2)
+
 boot_sd_ci <- function(x, confidence = 95, itr = 1000) {
   
   # init iterations and sample
@@ -37,7 +39,7 @@ boot_sd_ci <- function(x, confidence = 95, itr = 1000) {
   
 }
 
-mergeResults <- function(path, keyword = c("Results", "Snapshot")) {
+mergeResults <- function(path, keyword = c("Results", "Snapshot"), Pattern) {
   
   # get the directory content
   # content <- list.files(paste("~/", path, sep = ""))
@@ -47,7 +49,7 @@ mergeResults <- function(path, keyword = c("Results", "Snapshot")) {
   content <- content[order(content)]
   
   # only the folders
-  content <- grep(pattern = c("choosing"), x = content, value = T)
+  content <- grep(pattern = Pattern, x = content, value = T)
   
   # loop over the folders
   for (i in 1:length(content)) {
@@ -112,7 +114,7 @@ mergeResults <- function(path, keyword = c("Results", "Snapshot")) {
   
 } # end of function
 
-statsResults <- function(path, keyword = c("Results", "Snapshot")) {
+statsResults <- function(path, keyword = c("Results", "Snapshot"), Pattern) {
   
   # get the directory content
   # content <- list.files(paste("~/", path, sep = ""))
@@ -122,7 +124,7 @@ statsResults <- function(path, keyword = c("Results", "Snapshot")) {
   content <- content[order(content)]
   
   # only the folders
-  content <- grep(pattern = c("choosing"), x = content, value = T)
+  content <- grep(pattern = c(Pattern), x = content, value = T)
 
   # loop over the folders
   for (i in 1:length(content)) {
@@ -182,9 +184,49 @@ statsResults <- function(path, keyword = c("Results", "Snapshot")) {
     
     # write in the corresponding folder
     write.csv(tab, file = paste(folder, "/stats", keyword, "-", content[i], ".csv", sep = ""), row.names = FALSE)
-    
-  } # end of loop over folders
-} # end of function
+
+    # plot and save figure
+    {pdf(file = paste(folder, "/stats", keyword, "-", content[i], ".pdf", sep = ""), width = par('din')[1], height = par('din')[2])
+  
+      {data <- tab
+
+        x = data$timeStep
+        y1 = data$prey1PopulationSizeMean
+        y2 = data$prey2PopulationSizeMean
+        y3 = data$predator1PopulationSizeMean
+        y1min = data$prey1PopulationSizeICinf
+        y2min = data$prey2PopulationSizeICinf
+        y3min = data$predator1PopulationSizeICinf
+        y1max = data$prey1PopulationSizeICsup
+        y2max = data$prey2PopulationSizeICsup
+        y3max = data$predator1PopulationSizeICsup
+        y1c = "red"
+        y2c = "blue"
+        y3c = "orange"
+        tIntro = 210
+
+        fig <- ggplot(data, aes(x)) + 
+          geom_rect(aes(xmin = 0, xmax = tIntro, ymin = 0, ymax = 1.05*max(data$prey2PopulationSizeMean)), alpha=0.5, fill = "lightgrey") +
+          geom_ribbon(aes(ymin = y1min, ymax = y1max), alpha = 0.2, size = 0.1, col = y1c, fill = y1c) +
+          geom_ribbon(aes(ymin = y2min, ymax = y2max), alpha = 0.2, size = 0.1, col = y2c, fill = y2c) +
+          geom_ribbon(aes(ymin = y3min, ymax = y3max), alpha = 0.2, size = 0.1, col = y3c, fill = y3c) +
+          geom_line(aes(y = y1), color = y1c) +
+          geom_line(aes(y = y2), color = y2c) +
+          geom_line(aes(y = y3), color = y3c) +
+          # geom_point(aes(y = y1), size = 2.5, shape = 21, fill = "white", color = y1c) +
+          # geom_point(aes(y = y2), size = 2.5, shape = 22, fill = "white", color = y2c) +
+          # geom_point(aes(y = y3), size = 2.5, shape = 24, fill = "white", color = y3c) +
+          labs(x = "Time steps", y = "Population size") +
+          scale_colour_manual(name='Populations',
+                            breaks=c('Prey 1', 'Prey 2', 'Predator'),
+                            values=c(y1c, y2c, y3c))
+        # fig
+      }
+      dev.off()
+    } # end of plot
+
+    } # end of loop over folders
+  } # end of function
 
 getPosStab <- function(array, threshold, period) {
   
@@ -210,7 +252,7 @@ getPosStab <- function(array, threshold, period) {
   return(posStab)
 }
 
-posStabNoPred <- function(path, keyword = c("Results", "Snapshot"), stabThreshold, stabPeriod) {
+posStabNoPred <- function(path, keyword = c("Results", "Snapshot"), Pattern, stabThreshold, stabPeriod) {
   
   # get the directory content
   # content <- list.files(paste("~/", path, sep = ""))
@@ -220,7 +262,7 @@ posStabNoPred <- function(path, keyword = c("Results", "Snapshot"), stabThreshol
   content <- content[order(content)]
   
   # only the folders
-  content <- grep(pattern = c("choosing"), x = content, value = T)
+  content <- grep(pattern = c(Pattern), x = content, value = T)
     
   ## create table
   # new headers
@@ -273,22 +315,20 @@ posStabNoPred <- function(path, keyword = c("Results", "Snapshot"), stabThreshol
   colnames(tab) <- newHeaders
   
   # write in the corresponding folder
-  write.csv(tab, file = paste(path, "equilibriumNoPred.csv", sep = "/"), row.names = FALSE)
+  write.csv(tab, file = paste(folder, "equilibriumNoPred.csv", sep = "/"), row.names = FALSE)
 
 } # end of function
 
-# path to the simulation folder: example
-setwd("/Users/adrianbach/Desktop/PhD/GitKraken/Chapter2model/Duthie_Sims/")
-
-# path = paste(getwd(), "higherPreyMaxConsume", sep="/")
-# path = paste(getwd(), "lowerSize", sep="/")
-path = paste(getwd(), "lowerSizeHigherMaxConsume", sep="/")
+# path = paste(getwd(), "Duthie_Sims", sep="/")
+path = "/Users/adrianbach/Desktop/PhD/GitKraken/Chapter2model/"
+# path = getwd()
 keyword = "Results"
+Pattern = "test-size"
 
-mergeResults(path, keyword)
-statsResults(path, keyword)
+mergeResults(path, keyword, Pattern)
+statsResults(path, keyword, Pattern)
 
 stabPeriod = 5
 stabThreshold = 10
 
-posStabNoPred(path, keyword, stabThreshold, stabPeriod)
+posStabNoPred(path, keyword, Pattern, stabThreshold, stabPeriod)
