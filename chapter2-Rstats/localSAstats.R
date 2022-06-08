8de0a460e5399bf8d50736c9e54a2d7acccce5c9
Adrian Bach <adrian.bach@stir.ac.uk>
2022-06-08 19:42:59 +0100

    New figure for density before after
    still need to plot 4out of five figures

--- chapter2-Rstats/localSAstats.R
+++ chapter2-Rstats/localSAstats.R
@@ -1105,3 +1105,55 @@ fig <- ggplot(data, aes(x)) +
 
 # save plot in this folder
 ggsave(filename = "localSA-maxCons-catchRate.pdf", path = folderPath, plot = fig, width = 6.22, height = 5.73, limitsize = TRUE)
+
+#### New figures for density ####
+
+folderPath = "/Users/adrianbach/Desktop/PhD/GitKraken/Chapter2model/localSA/folder-localSA-offspringAvg/allStatsAndPlots/localSAfiles/"
+filePath = "/Users/adrianbach/Desktop/PhD/GitKraken/Chapter2model/localSA/folder-localSA-offspringAvg/allStatsAndPlots/localSAfiles/stats-folder-localSA-offspringAvg.csv"
+data <- read.csv(filePath)
+data <- subset(data, data$predSpecific == 0 & data$predOportunistic == 0)
+
+x = data$preyOffspRatio
+y1 = data$prey1densBeforeMean
+y2 = data$prey1densAfterMean
+y3 = data$prey2densBeforeMean
+y4 = data$prey2densAfterMean
+y1min = data$prey1densBeforeMin
+y2min = data$prey1densAfterMin
+y3min = data$prey2densBeforeMin
+y4min = data$prey2densAfterMin
+y1max = data$prey1densBeforeMax
+y2max = data$prey1densAfterMax
+y3max = data$prey2densBeforeMax
+y4max = data$prey2densAfterMax
+y1c = "pink"
+y2c = "red"
+y3c = "cyan"
+y4c = "blue"
+# tIntro = 210
+
+fig <- ggplot(data, aes(x)) + 
+  # geom_hline(yintercept = 0, color = "darkgreen", linetype = "dashed") +
+  # geom_hline(yintercept = -1, color = "darkred", linetype = "dashed") +
+  geom_hline(yintercept = y1[1], alpha = 0.5, color = "darkred", linetype = "dashed") +
+  geom_hline(yintercept = y3[1], alpha = 0.5, color = "darkblue", linetype = "dashed") +
+  geom_line(aes(y = y1), color = y1c, alpha = 0.2, position = position_nudge(x = -0.15)) +
+  geom_line(aes(y = y2), color = y2c, alpha = 0.2, position = position_nudge(x = 0.05)) +
+  geom_line(aes(y = y3), color = y3c, alpha = 0.2, position = position_nudge(x = -0.05)) +
+  geom_line(aes(y = y4), color = y4c, alpha = 0.2, position = position_nudge(x = 0.15)) +
+  # geom_rect(aes(xmin = 0, xmax = tIntro, ymin = 0, ymax = 1.05*max(data$prey2PopulationSizeMean)), alpha=0.5, fill = "lightgrey") +
+  geom_pointrange(aes(y = y1, ymin = y1min, ymax = y1max), shape = 24, fill = "white", size = 0.8, col = y1c, position = position_nudge(x = -0.15)) +
+  geom_pointrange(aes(y = y2, ymin = y2min, ymax = y2max), shape = 25, fill = "white", size = 0.8, col = y2c, position = position_nudge(x = 0.05)) +
+  geom_pointrange(aes(y = y3, ymin = y3min, ymax = y3max), shape = 24, fill = "white", size = 0.8, col = y3c, position = position_nudge(x = -0.05)) +
+  geom_pointrange(aes(y = y4, ymin = y4min, ymax = y4max), shape = 25, fill = "white", size = 0.8, col = y4c, position = position_nudge(x = 0.15)) +
+  # geom_ribbon(aes(ymin = y3min, ymax = y3max), alpha = 0.2, size = 0.1, col = y3c, fill = y3c) +
+  # geom_point(aes(y = y1), size = 2.5, shape = 24, fill = "white", color = y1c, position = position_nudge(x = -0.1)) +
+  # geom_point(aes(y = y2), size = 2.5, shape = 25, fill = "white", color = y2c, position = position_nudge(x = 0.1)) # +
+  # geom_point(aes(y = y3), size = 2.5, shape = 24, fill = "white", color = y3c, position = position_nudge(x = 0.15)) +
+  labs(x = "Prey2 to prey1 average offspring ratio", y = "Average prey 2 to prey 1 density") # +
+# scale_colour_manual(name='Populations',
+#                     breaks=c('Prey 1', 'Prey 2', 'Predator'),
+#                     values=c(y1c, y2c, y3c))
+
+# save plot in this folder
+ggsave(filename = "localSA-offsprinAvg-prey2toPrey1densDev.pdf", path = folderPath, plot = fig, width = 6.22, height = 5.73, limitsize = TRUE)
