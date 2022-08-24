df3f41331f0213448fa1d209d7d4f88e325e74af
AdrianBach <43581266+AdrianBach@users.noreply.github.com>
2022-08-24 17:19:28 +0200

    adding prey times of introduction
    needs testing

--- launchReplicatesSim.sh
+++ launchReplicatesSim.sh
@@ -38,28 +38,30 @@ pry_offs_1=1    # argv[15] prey 1 max number of offspring
 pry_offs_2=1    # argv[16] prey 2 max number of offspring
 # pry_repr_1=5  # argv[17] prey 1 resource units needed to pass reproduction trial
 # pry_repr_2=5  # argv[18] prey 2 resource units needed to pass reproduction trial
+pry_intro_1=0   # argv[19]
+pry_intro_2=0   # argv[20]
 
 # predator variables
-prd_nb=1        # argv[19] number of predator types
-prd_init_1=5    # argv[20] predator 1 initial density in nb of individuals
-prd_move_1=0.1  # argv[21] predator 1 max movement range in fraction of size
-prd_offs_1=1    # argv[24] predator 1 max number of offspring
-prd_intr_1=0    # argv[26] predator 1 time of introduction in the model
+prd_nb=1        # argv[21] number of predator types
+prd_init_1=5    # argv[22] predator 1 initial density in nb of individuals
+prd_move_1=0.1  # argv[23] predator 1 max movement range in fraction of size
+prd_offs_1=1    # argv[26] predator 1 max number of offspring
+prd_intr_1=0    # argv[28] predator 1 time of introduction in the model
 # prd_asym_1=1  # argv[] asymmetry in prey1 to prey2 conversion rates
-prd_ctch_pry1_1=0.25  # argv[27] predator 1 prey1 catch probability
-prd_ctch_pry2_1=0.25  # argv[28] predator 1 prey2 catch probability
-prd_oprt_1=0    # argv[31] is predator oportunistic? (0 or 1)
-prd_spcf_1=1    # argv[32] is predator specific? (0 or 1)
+prd_ctch_pry1_1=0.25  # argv[29] predator 1 prey1 catch probability
+prd_ctch_pry2_1=0.25  # argv[30] predator 1 prey2 catch probability
+prd_oprt_1=0    # argv[33] is predator oportunistic? (0 or 1)
+prd_spcf_1=1    # argv[34] is predator specific? (0 or 1)
 
 # time variables
-simu_time=20    # argv[33] simulation time
-freq_repr=10    # argv[34] frequency of reproduction trials
-freq_surv=$freq_repr    # argv[35] frequency of survival trials
-freq_rfll=$freq_repr    # argv[36] frequency of landscape resources refill
+simu_time=20    # argv[35] simulation time
+freq_repr=10    # argv[36] frequency of reproduction trials
+freq_surv=$freq_repr    # argv[37] frequency of survival trials
+freq_rfll=$freq_repr    # argv[38] frequency of landscape resources refill
 
 # frequency of assessment
-freq_rslt=1    # argv[37] frequency of landscape results shot
-freq_snap=101  # argv[38] frequency of snap measure
+freq_rslt=1    # argv[39] frequency of landscape results shot
+freq_snap=101  # argv[40] frequency of snap measure
 
 # number of replicates
 rep=1
@@ -77,12 +79,12 @@ divide=$freq_surv*$pry_cons_2; by=3; pry_surv_2=`echo "scale=0; ($divide+$by-1)/
 pry_repr_1=$pry_surv_1; # argv[17] prey 1 resource units needed to pass reproduction trial. Defined as a proportion of what is needed to pass survival trial.
 pry_repr_2=$pry_surv_2; # argv[18] prey 2 resource units needed to pass reproduction trial
 
-prd_cons_1=$((3*$pry_cons_1)) # arg[22]
-divide=$prd_cons_1*$freq_surv; by=3; prd_surv_1=`echo "scale=0; ($divide+$by-1)/$by" | bc`; # arg[23]
-prd_repr_1=$prd_surv_1; # echo "prd_repr_1 = $prd_repr_1" # argv[25] predator 1 resource units needed to pass reproduction trial. Defined as a proportion of what is needed to pass survival trial.
-prd_cvrt_pry1_1=$(($freq_surv * $prd_cons_1/3))  # argv[29] predator 1 prey1 resources/catch
+prd_cons_1=$((3*$pry_cons_1)) # arg[24]
+divide=$prd_cons_1*$freq_surv; by=3; prd_surv_1=`echo "scale=0; ($divide+$by-1)/$by" | bc`; # arg[25]
+prd_repr_1=$prd_surv_1; # echo "prd_repr_1 = $prd_repr_1" # argv[27] predator 1 resource units needed to pass reproduction trial. Defined as a proportion of what is needed to pass survival trial.
+prd_cvrt_pry1_1=$(($freq_surv * $prd_cons_1/3))  # argv[31] predator 1 prey1 resources/catch
 ratio=1
-prd_cvrt_pry2_1=$(($ratio*$prd_cvrt_pry1_1))  # argv[30] predator 1 prey1 resources/catch
+prd_cvrt_pry2_1=$(($ratio*$prd_cvrt_pry1_1))  # argv[32] predator 1 prey1 resources/catch
 
 # name the simulation with only the variables of interest and their value
 sim_name="test-size$size-simTime$simu_time-res1max$max_res_1-predOprt$prd_oprt_1-predSpcf$prd_spcf_1-pry1cons$pry_cons_1-prdSurv$prd_surv_1-prdCtchProb1$prd_ctch_pry1_1-prdCtchProb2$prd_ctch_pry2_1" # argv[1]
@@ -160,6 +162,10 @@ printf "pry_repr_1 = $pry_repr_1 \t # argv[$p] prey 1 resource units needed to p
 p=$(($p+1))
 printf "pry_repr_2 = $pry_repr_2 \t # argv[$p] prey 2 resource units needed to pass reproduction trial\n\n" >> paramFile.txt
 p=$(($p+1))
+printf "pry_intro_1 = $pry_intro_1 \t # argv[$p] prey 1 time step of introduction \n" >> paramFile.txt
+p=$(($p+1))
+printf "pry_intro_2 = $pry_intro_2 \t # argv[$p] prey 2 time step of introduction \n\n" >> paramFile.txt
+p=$(($p+1))
 printf "# predator variables\n" >> paramFile.txt
 printf "prd_nb = $prd_nb \t # argv[$p] number of predator types\n" >> paramFile.txt
 p=$(($p+1))
@@ -224,7 +230,7 @@ do
     # echo "rand seed: $rand_seed" 
 
     start=$(date +%s)
-    ./test-chapter2ibm.o $sim_name $size $res_nb $max_res_1 $max_res_2 $pry_nb $pry_init_1 $pry_init_2 $pry_move_1 $pry_move_2 $pry_cons_1 $pry_cons_2 $pry_surv_1 $pry_surv_2 $pry_offs_1 $pry_offs_2 $pry_repr_1 $pry_repr_2 $prd_nb $prd_init_1 $prd_move_1 $prd_cons_1 $prd_surv_1 $prd_offs_1 $prd_repr_1 $prd_intr_1 $prd_ctch_pry1_1 $prd_ctch_pry2_1 $prd_cvrt_pry1_1 $prd_cvrt_pry2_1 $prd_oprt_1 $prd_spcf_1 $simu_time $freq_repr $freq_surv $freq_rfll $freq_rslt $freq_snap $rand_seed
+    ./test-chapter2ibm.o $sim_name $size $res_nb $max_res_1 $max_res_2 $pry_nb $pry_init_1 $pry_init_2 $pry_move_1 $pry_move_2 $pry_cons_1 $pry_cons_2 $pry_surv_1 $pry_surv_2 $pry_offs_1 $pry_offs_2 $pry_repr_1 $pry_repr_2 $pry_intro_1 $pry_intro_2 $prd_nb $prd_init_1 $prd_move_1 $prd_cons_1 $prd_surv_1 $prd_offs_1 $prd_repr_1 $prd_intr_1 $prd_ctch_pry1_1 $prd_ctch_pry2_1 $prd_cvrt_pry1_1 $prd_cvrt_pry2_1 $prd_oprt_1 $prd_spcf_1 $simu_time $freq_repr $freq_surv $freq_rfll $freq_rslt $freq_snap $rand_seed
     end=$(date +%s)
 
     time_s=$(($end-$start))
